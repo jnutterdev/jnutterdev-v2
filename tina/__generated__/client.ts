b4f3844059666055fead911db94fb70424fe1e66
jnutterdev <hello@jnutterdev.com>
2026-05-26 15:39:03 -0400

    Initial build

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types.js";
+export const client = createClient({ cacheDir: '/Users/chromia/github.com/jnutterdev-v2/tina/__generated__/.cache/1779824215597', url: 'https://content.tinajs.io/2.4/content/a8aeec1b-df81-4fdb-86f4-eb67473460b7/github/main', token: 'eb95adaf1ac8ad65296f7aed1d010ff293ce9ada', queries,  });
+export default client;
+  
---
+++ tina/__generated__/types.js
@@ -0,0 +1,192 @@
+export function gql(strings, ...args) {
+  let str = "";
+  strings.forEach((string, i) => {
+    str += string + (args[i] || "");
+  });
+  return str;
+}
+export const ProjectPartsFragmentDoc = gql`
+    fragment ProjectParts on Project {
+  __typename
+  title
+  description
+  tech
+  status
+  github_url
+  live_url
+  thumbnail
+  order
+  featured
+  body
+}
+    `;
+export const PostPartsFragmentDoc = gql`
+    fragment PostParts on Post {
+  __typename
+  title
+  date
+  excerpt
+  tags
+  cover_image
+  body
+}
+    `;
+export const ProjectDocument = gql`
+    query project($relativePath: String!) {
+  project(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...ProjectParts
+  }
+}
+    ${ProjectPartsFragmentDoc}`;
+export const ProjectConnectionDocument = gql`
+    query projectConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: ProjectFilter) {
+  projectConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...ProjectParts
+      }
+    }
+  }
+}
+    ${ProjectPartsFragmentDoc}`;
+export const PostDocument = gql`
+    query post($relativePath: String!) {
+  post(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...PostParts
+  }
+}
+    ${PostPartsFragmentDoc}`;
+export const PostConnectionDocument = gql`
+    query postConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: PostFilter) {
+  postConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...PostParts
+      }
+    }
+  }
+}
+    ${PostPartsFragmentDoc}`;
+export function getSdk(requester) {
+  return {
+    project(variables, options) {
+      return requester(ProjectDocument, variables, options);
+    },
+    projectConnection(variables, options) {
+      return requester(ProjectConnectionDocument, variables, options);
+    },
+    post(variables, options) {
+      return requester(PostDocument, variables, options);
+    },
+    postConnection(variables, options) {
+      return requester(PostConnectionDocument, variables, options);
+    }
+  };
+}
+import { createClient } from "tinacms/dist/client";
+const generateRequester = (client) => {
+  const requester = async (doc, vars, options) => {
+    let url = client.apiUrl;
+    if (options?.branch) {
+      const index = client.apiUrl.lastIndexOf("/");
+      url = client.apiUrl.substring(0, index + 1) + options.branch;
+    }
+    const data = await client.request({
+      query: doc,
+      variables: vars,
+      url
+    }, options);
+    return { data: data?.data, errors: data?.errors, query: doc, variables: vars || {} };
+  };
+  return requester;
+};
+export const ExperimentalGetTinaClient = () => getSdk(
+  generateRequester(
+    createClient({
+      url: "https://content.tinajs.io/2.4/content/a8aeec1b-df81-4fdb-86f4-eb67473460b7/github/main",
+      queries
+    })
+  )
+);
+export const queries = (client) => {
+  const requester = generateRequester(client);
+  return getSdk(requester);
+};
