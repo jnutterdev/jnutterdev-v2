55aff41f690a1987c163446d742be9f4cc849484
jnutterdev <hello@jnutterdev.com>
2026-06-14 17:43:01 -0400

    setting up tina to handle images

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'd470cf720b9398069f484c4795939d3b28c19072', queries,  });
+export const client = createClient({ cacheDir: '/Users/john/github.com/jnutterdev/jnutterdev-v2/tina/__generated__/.cache/1781473349089', url: 'https://content.tinajs.io/2.4/content/108ffc85-6653-4423-a46e-eaa9c2063fcc/github/main', token: 'd470cf720b9398069f484c4795939d3b28c19072', queries,  });
 export default client;
   
