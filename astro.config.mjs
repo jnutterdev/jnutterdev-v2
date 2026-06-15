264bc64707ab1aff92e7ff800f404bb7ca97a16f
jnutterdev <hello@jnutterdev.com>
2026-06-15 13:03:19 -0400

    fixing site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://jnutterdev.com',
+  site: 'https://jnutter.dev',
   output: 'static',
   integrations: [mdx()],
 });
